87f8ba27af17c05e3235a4211eb1eab72c693cde
Coder Sathy <69349524+CoderSathy@users.noreply.github.com>
2020-10-18 16:17:56 +0545

    Add files via upload

---
+++ app/app/api.py
@@ -0,0 +1,88 @@
+from flask import Flask, jsonify, make_response, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask import render_template
+
+import json
+import random
+app = Flask(__name__)
+
+# Prevents the json data to sorted alphabetically
+app.config['JSON_SORT_KEYS'] = False
+
+# limit an ip by 100request per hour
+limiter = Limiter(app, key_func=get_remote_address,
+                  default_limits=["100 per hour"])
+
+with open('greetings/index.json', 'r') as file:
+    greetings = json.load(file)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+    #return "Try: /api/greetings/ , /api/greetings/id/ , /api/greetings/random"
+
+
+@app.route('/api/greetings/')
+def greetings_all():
+    return jsonify(greetings)
+
+
+@app.route('/api/greetings/lang/<language>/')
+def greeting_language(language):
+    data = []
+    for greeting in greetings:
+        if greeting['language'] == language.lower():
+            data.append(greeting)
+    return jsonify(data)
+
+
+@app.route('/api/greetings/lang/<language>/random/')
+def greeting_language_random(language):
+    data = []
+    for greeting in greetings:
+        if greeting['language'] == language.lower():
+            data.append(greeting)
+    try:
+        return jsonify(random.choice(data))
+    except IndexError:
+        return jsonify({"Error": {"Code": 404, "Message": "Greeting with the given language was not found."}}), 404
+
+
+@app.route('/api/greetings/<int:id>/')
+def greeting_id(id):
+    try:
+        greeting = greetings[id]
+        return jsonify(greeting)
+    except IndexError:
+        return jsonify({"Error": {"Code": 404, "Message": "Greeting with the given id was not found."}}), 404
+
+
+@app.route('/api/greetings/random/')
+def greeting_random():
+    greeting = random.choice(greetings)
+    return jsonify(greeting)
+
+@app.route('/local')
+def local_api_info():
+    return "Try: /api/greetings/ , /api/greetings/id/ , /api/greetings/random"
+
+# test
+@app.route('/ping')
+def pong():
+    return "Pong"
+
+# error handler for 429
+
+
+@app.errorhandler(429)
+def ratelimit_handler(e):
+    return make_response(jsonify(error=f"rate limit exceeded {e.description}"))
+
+# Whitelist the local ip
+
+
+@limiter.request_filter
+def ip_whitelist():
+    return request.remote_addr == '127.0.0.1'
